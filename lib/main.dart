70f90ee23d12fc6f9454e3e41c9a043705feaa2b
13octt <88072761+13octt@users.noreply.github.com>
2023-08-30 14:57:46 +0700

    create note app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget{
+
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }
 
