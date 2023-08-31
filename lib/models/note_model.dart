9aad1e8d95ea7201f8ebfe8cc05915e5a588884b
13octt <88072761+13octt@users.noreply.github.com>
2023-08-31 15:33:49 +0700

    init hive and open box

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,20 @@
+import 'package:hive/hive.dart';
 
-class NoteModel {
+part 'note_model.g.dart';
+
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject{
+  @HiveField(0)
   late final String title;
+
+  @HiveField(1)
   late final String subTitle;
+
+  @HiveField(2)
   late final String date;
+
+  @HiveField(3)
   late final int color;
 
   NoteModel({
