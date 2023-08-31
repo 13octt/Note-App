54144d92c67ef87807a65ad52da50396ed99cf53
13octt <88072761+13octt@users.noreply.github.com>
2023-08-31 10:57:58 +0700

    add hive to project

---
+++ lib/models/note_models.dart
@@ -0,0 +1,16 @@
+
+class NoteModel {
+  late final String title;
+  late final String subTitle;
+  late final String date;
+  late final int color;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.color,
+  });
+
+
+}
--- lib/views/widgets/edit_view_body.dart
+++ lib/views/widgets/edit_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
+import 'package:note_app/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -7,16 +8,23 @@ class EditNoteViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0),
-      child: Column(children: [
-        SizedBox(
-          height: 50,
-        ),
-        CustomAppBar(
-          title: 'Edit Note View',
-          icon: Icons.check,
-        ),
-      ]),
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: SingleChildScrollView(
+        child: Column(children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(
+            title: 'Edit Note View',
+            icon: Icons.check,
+          ),
+          CustomTextField(hint: 'Title', maxLines: 1),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(hint: 'Content', maxLines: 5),
+        ]),
+      ),
     );
   }
 }
